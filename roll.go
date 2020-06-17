765e6de6c0d5776a2140a21104ce6bda86d69510
christophberger <christophberger@users.noreply.github.com>
2020-06-17 12:08:14 +0200

    Add RollMulti

--- roll.go
+++ roll.go
@@ -9,6 +9,14 @@ func Roll(sides int) int {
 	return rand.Intn(sides) + 1
 }
 
+func RollMulti(num, sides int) int {
+	sum := 0
+	for i := 0; i < num; i++ {
+		sum += Roll(sides)
+	}
+	return sum
+}
+
 func Seed(n int64) {
 	if n == 0 {
 		rand.Seed(time.Now().UnixNano())
